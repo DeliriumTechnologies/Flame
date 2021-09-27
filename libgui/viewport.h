2e089be7e0d749cfd71099eba6cc992109b1b9ff
Jonas <jonaskorte2904@gmail.com>
2021-09-27 09:02:00 +0200

    Ported GUI Code to External Library (libgui.dll)

---
+++ libgui/viewport.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "api.h"
+#include <QOpenGLWidget>
+
+enum EXPORTED ViewportShadingMode 
+{
+    VSM_WIRE,
+    VSM_DEFAULT,
+    VSM_LOOKDEV,
+    VSM_RENDERED
+};
+
+class EXPORTED Viewport : public QOpenGLWidget
+{
+public:
+    Viewport(QWidget *parent);
+private:
+    ViewportShadingMode m_shadingMode;
+
+};
