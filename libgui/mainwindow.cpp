d45d42d20239e7a5e6a4d170c28552c14a791eaf
JonasKorte <jonaskorte2904@gmail.com>
2021-09-27 15:50:24 +0200

    Switched to Qt5, Added Custom Cursor

--- libgui/mainwindow.cpp
+++ libgui/mainwindow.cpp
@@ -1,23 +1,37 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     this->setWindowTitle("Phoenix3D 1.0");
     this->setWindowState(Qt::WindowState::WindowMaximized);
-    this->setBaseSize(1280, 720);
+    this->setBaseSize(1920, 1080);
+    this->setCursor(QCursor(QPixmap(":/res/img/cursor.png")));
+    this->setObjectName("_main_window");
 
     this->m_layout = new QGridLayout(this);
 
     this->setLayout(this->m_layout);
 
     this->m_menuBar = new QMenuBar(this);
+    this->m_menuBar->setObjectName("_main_menu_bar");
 
-    QMenu* fileMenu = new QMenu("File", this->m_menuBar);
-    QMenu* editMenu = new QMenu("Edit", this->m_menuBar);
-    QMenu* viewMenu = new QMenu("View", this->m_menuBar);
-    QMenu* nodeMenu = new QMenu("Node", this->m_menuBar);
-    QMenu* renderMenu = new QMenu("Render", this->m_menuBar);
-    QMenu* helpMenu = new QMenu("Help", this->m_menuBar);
+    QMenu *fileMenu = new QMenu("File", this->m_menuBar);
+    fileMenu->setObjectName("_file_menu");
+
+    QMenu *editMenu = new QMenu("Edit", this->m_menuBar);
+    editMenu->setObjectName("_edit_menu");
+
+    QMenu *viewMenu = new QMenu("View", this->m_menuBar);
+    viewMenu->setObjectName("_view_menu");
+
+    QMenu *nodeMenu = new QMenu("Node", this->m_menuBar);
+    nodeMenu->setObjectName("_node_menu");
+
+    QMenu *renderMenu = new QMenu("Render", this->m_menuBar);
+    renderMenu->setObjectName("_render_menu");
+
+    QMenu *helpMenu = new QMenu("Help", this->m_menuBar);
+    helpMenu->setObjectName("_help_menu");
 
     this->m_menuBar->addMenu(fileMenu);
     this->m_menuBar->addMenu(editMenu);
@@ -44,12 +58,12 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
 
         this->m_menuBar->setStyleSheet(data);
     }
-    else 
+    else
     {
         std::cerr << "Failed to load stylesheet at qrc:/res/qts/menuBar.qts!" << std::endl;
     }
 
-    QGraphicsDropShadowEffect* shadow = new QGraphicsDropShadowEffect(this->m_menuBar);
+    QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect(this->m_menuBar);
 
     shadow->setBlurRadius(10.0);
     shadow->setColor(QColor::fromRgb(0, 0, 0, 128));
--- phoenix/main.cpp
+++ phoenix/main.cpp
@@ -3,18 +3,19 @@
 
 #include "mainwindow.h"
 
-int main(int argc, char* argv[]) 
+int main(int argc, char *argv[])
 {
     std::cout << "Phoenix3D 1.0 by Jonas Korte." << std::endl;
 
     QApplication app(argc, argv);
+    app.setOverrideCursor(QCursor(QPixmap(":/res/img/cursor.png")));
+    app.setObjectName("_application");
 
     QWidget root;
+    root.setObjectName("_application_root");
 
     MainWindow window(&root);
-
     window.setStyleSheet("QMainWindow { background-color: #3d3d3d }");
-
     window.show();
 
     return app.exec();
