b0d5a623c8a3eb1e542a38414c9dc2041952e2a9
Delirium Technologies <59111455+JonasKorte@users.noreply.github.com>
2021-09-29 15:44:30 +0000

    Added Icon Loading

--- libgui/mainwindow.cpp
+++ libgui/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
     this->setWindowState(Qt::WindowState::WindowMaximized);
     this->setBaseSize(1920, 1080);
     this->setCursor(QCursor(QPixmap(":/res/img/cursor.png")));
+    this->setWindowIcon(QIcon(":/res/imglogo.jpg"));
     this->setObjectName("_main_window");
 
     this->m_layout = new QGridLayout(this);
